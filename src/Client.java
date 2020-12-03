2def6f233eab2aa04e2c9b23b0b951b1def29588
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-12-02 20:40:17 -0600

    Added comments to the code

--- src/Client.java
+++ src/Client.java
@@ -3,8 +3,11 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+
+        // Establishing a socket
         Socket socket = new Socket("localhost", 9000);
 
+        // Accessing socket's input/output streams and keyboard input
         InputStream inputStream = socket.getInputStream();
         DataInputStream dataInputStream = new DataInputStream(inputStream);
 
@@ -13,30 +16,44 @@ public class Client {
 
         BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Connected to the server");
-        System.out.println("Type \"exit\" to exit");
+        // If connection is successful, output a confirmation message to Client
+        if(socket.isConnected()) {
+            //Confirmation message
+            System.out.println("Connected to the server");
+            System.out.println("Type \"exit\" to exit");
 
-        String messageFromServer, messageFromClient = "";
-        while(!messageFromClient.equals("exit")){
-            while(true){
-                messageFromServer = dataInputStream.readUTF();
-                if(messageFromServer.equals("--end--")) {
-                    break;
+            // Interaction between Server and Client
+            // Executes continuously until Client types "exit"
+            String messageFromServer, messageFromClient = "";
+            while (!messageFromClient.equals("exit")) {
+
+                // Reading and displaying all messages from Server
+                while (true) {
+                    messageFromServer = dataInputStream.readUTF();
+
+                    // Different scenarios to navigate the flow of the program operation
+                    // --end-- signifies the end of the messages from Server
+                    // --close connection-- triggers socket.close() as requested by Server
+                    if (messageFromServer.equals("--end--")) {
+                        break;
+                    }
+                    if (messageFromServer.equals("--close connection--")) {
+                        messageFromClient = "exit";
+                        break;
+                    }
+                    System.out.println("(Server -> Client) " + messageFromServer);
                 }
-                if(messageFromServer.equals("--close connection--")){
-                    messageFromClient = "exit";
+                if (messageFromClient.equals("exit"))
                     break;
-                }
-                System.out.println("(Server -> Client) " + messageFromServer);
+
+                // Reading Client's keyboard input
+                messageFromClient = keyboard.readLine();
+
+                // Sending Client's input to Server
+                dataOutputStream.writeUTF(messageFromClient);
+                dataOutputStream.flush();
             }
-            if(messageFromClient.equals("exit"))
-                break;
-            messageFromClient = keyboard.readLine();
-            dataOutputStream.writeUTF(messageFromClient);
-            dataOutputStream.flush();
+            System.out.println("Connection is closed. Good bye!");
         }
-        System.out.println("Connection is closed. Good bye!");
-        //Bye
-        //Inside encryption_protocol
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -5,19 +5,25 @@ import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+
+        //Establishing a server
         int port = 9000;
         ServerSocket serverSocket = new ServerSocket(port);
         while(true){
+
+            // Accepting a connection
             System.out.println("About to accept a connection...");
             Socket clientSocket = serverSocket.accept();
             System.out.println("Accepted connection from: " + clientSocket);
 
+            // Accessing clientSocket's input/output stream
             InputStream inputStream = clientSocket.getInputStream();
             DataInputStream dataInputStream = new DataInputStream(inputStream);
 
             OutputStream outputStream = clientSocket.getOutputStream();
             DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
 
+            // Login protocol
             dataOutputStream.writeUTF("Hello Client! Please, login first to use the chat.");
             dataOutputStream.writeUTF("Enter username:");
             dataOutputStream.writeUTF("--end--");
@@ -27,29 +33,48 @@ public class Server {
             dataOutputStream.writeUTF("--end--");
             dataOutputStream.flush();
             String password = dataInputStream.readUTF();
-
             boolean isLoggedIn = login(username, password);
+
+            // If...else statement to direct the flow of the program operation
+            // If user is logged in, then output a confirmation message and start the chat
+            // else output an error message and close the connection
             if(!isLoggedIn){
                 dataOutputStream.writeUTF("Error: Incorrect username and/or password. Please, try again later. Good bye!");
                 dataOutputStream.writeUTF("--close connection--");
+                dataOutputStream.flush();
             }else {
+                // Confirmation message
                 dataOutputStream.writeUTF("Successful connection");
                 dataOutputStream.writeUTF("--end--");
+                dataOutputStream.flush();
+
+                // Interaction between Server and Client
+                // Executes continuously until Client types "exit"
                 String messageFromClient = "";
                 while (!messageFromClient.equals("exit")) {
+
+                    // Receiving message from Client
                     messageFromClient = dataInputStream.readUTF();
+
+                    // Echoeing the message back to Client
                     dataOutputStream.writeUTF("You typed: " + messageFromClient);
                     dataOutputStream.writeUTF("--end--");
                     dataOutputStream.flush();
                     if (messageFromClient.equals("exit"))
                         break;
                 }
+
+                // If Client types "exit", close the connection
                 clientSocket.close();
             }
         }
     }
 
     public static boolean login(String username, String password){
+
+        // Login protocol
+        // It compares username and password provided by Client to
+        // the information stored in "users.txt"
         File file = new File("users.txt");
         try{
             if(!file.exists()){
